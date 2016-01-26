c6d2fd14f534e90c224271714dac997774d3f76b
CirFi <i@cirfi.com>
2016-01-26 13:41:30 +0800

    1.6, 1.7

---
+++ 1.6.scm
@@ -0,0 +1,4 @@
+;;; new-if is a procedure
+;;; so all sub-expressions will be called
+;;; ignoring the true or false predicate
+;;; because Scheme is applicative-order
---
+++ 1.7.scm
@@ -0,0 +1,17 @@
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+		 x)))
+(define (improve guess x)
+  (average guess (/ x guess)))
+(define (average x y)
+  (/ (+ x y) 2))
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+;;; new good-enough?
+(define (good-enough? guess x)
+  (< (/ (abs (- (improve guess x)
+		guess))
+	guess)
+     0.001))
