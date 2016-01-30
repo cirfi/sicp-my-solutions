eedf57f988d3da1bf33a015b7feb4270cfbb0153
CirFi <i@cirfi.com>
2016-01-30 17:11:05 +0800

    1.16~1.19

---
+++ 1.19.scm
@@ -0,0 +1,18 @@
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+(define (fib-iter a b p q count)
+  (define (square n)
+    (* n n))
+  (cond ((= count 0) b)
+	((even? count)
+	 (fib-iter a
+		   b
+		   (+ (square p) (square q))
+		   (+ (* 2 p q) (square q))
+		   (/ count 2)))
+	(else
+	 (fib-iter (+ (* b q) (* a q) (* a p))
+		   (+ (* b p) (* a q))
+		   p
+		   q
+		   (- count 1)))))
